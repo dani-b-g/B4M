cb1678b13ee11c7970981559ee7459edc19487bc
Daniel Barriga <daniel.barriga.grados@gmail.com>
2019-03-07 10:44:43 +0100

    .

--- application/views/mensajes_v.php
+++ application/views/mensajes_v.php
@@ -5,7 +5,7 @@
         unset($_SESSION['flashdata']); ?>
     </div>
     <?php endif; ?>
-    <div class="card">
+    <div class="card mb-2">
         <div class="card-header">
             Mensajes de:
             <?php echo $usuario; ?>
@@ -15,7 +15,9 @@
             <div class="card-text">
                 <ul class="list-group">
                     <?php foreach ($mensajes as $value) : ?>
-                    <li data-mensaje-type='<?php echo $value->id_men ?>' data-des-type='<?php echo $value->des_men ?>' id='<?php echo $value->rem_men ?>' class="list-group-item d-flex btn purple-gradient justify-content-between align-items-center mensajes">
+                    <li data-mensaje-type='<?php echo $value->id_men ?>' data-des-type='<?php echo $value->des_men ?>'
+                        id='<?php echo $value->rem_men ?>'
+                        class="list-group-item d-flex btn purple-gradient justify-content-between align-items-center mensajes">
                         <h5 id="remMen" data-rem-type="<?php echo $value->salida  ?>">
                             <?php echo $value->salida  ?>
                         </h5>
@@ -47,14 +49,6 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div id=mensajeAnterior>
-                    <div class="col">
-                        <h5>Mensaje anterior</h5>
-                        <blockquote class="blockquote">
-                            <p class="mb-0" id='last_men'></p>
-                        </blockquote>
-                    </div>
-                </div>
                 <div id="contenidoMen">
                     <div class="col">
                         <h3>Cuerpo del mensaje:</h3>
@@ -80,12 +74,14 @@
                     <input id="rem_men" name="rem_men" type="hidden" value="<?php echo $_SESSION['id_login'] ?>">
                     <div class="form-group">
                         <label for="titulo_men">Titulo</label>
-                        <input id="titulo_men" maxlength="50" name="titulo_men" class="form-control" type="text" required>
+                        <input id="titulo_men" maxlength="50" name="titulo_men" class="form-control" type="text"
+                            required>
                     </div>
                     <div class="form-group">
                         <div class="md-form amber-textarea active-amber-textarea-2">
                             <label class="" for="cuerpo_men">Cuerpo Mensaje</label>
-                            <textarea type="text" id="cuerpo_men" name="cuerpo_men" class="md-textarea form-control" rows="5" required></textarea>
+                            <textarea type="text" id="cuerpo_men" name="cuerpo_men" class="md-textarea form-control"
+                                rows="15" required></textarea>
                         </div>
                     </div>
             </div>
@@ -97,4 +93,4 @@
     </div>
 </div>
 <!-- Fin modal -->
-<script src="<?php echo base_url('assets/js/mensajes_v.js') ?>"></script> 
+<script src="<?php echo base_url('assets/js/mensajes_v.js') ?>"></script>
