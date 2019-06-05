0dc93e4c4bffed1bb1a1b209a46f70dcb2b7dccb
danijef8 <danijef8@hotmail.com>
2019-06-05 03:05:56 +0200

    arreglo bug con boton de borrar

--- application/views/avisos_v.php
+++ application/views/avisos_v.php
@@ -3,16 +3,16 @@
         <div class="col">
             <div id="feedback" class="alert alert-success" hidden></div>
             <?php if (isset($_SESSION['flashdata']) && isset($_SESSION['error'])) : ?>
-                <div class=" alert alert-info" role="alert">
-                    <?php echo $_SESSION['flashdata'];
+            <div class=" alert alert-info" role="alert">
+                <?php echo $_SESSION['flashdata'];
                     unset($_SESSION['flashdata']); ?>
-                </div>
+            </div>
             <?php endif; ?>
             <?php if (isset($_SESSION['flashdata']) && $_SESSION['login'] == true) : ?>
-                <div class="alert alert-info" role="alert">
-                    <?php echo $_SESSION['flashdata'];
+            <div class="alert alert-info" role="alert">
+                <?php echo $_SESSION['flashdata'];
                     unset($_SESSION['flashdata']); ?>
-                </div>
+            </div>
             <?php endif; ?>
         </div>
     </div>
@@ -24,36 +24,41 @@
         <h5 class="card-title"></h5>
         <div class="card-text">
             <?php foreach ($avisos as $aviso) : ?>
-                <div class="card border-secondary p-2 m-2">
-                    <div class="card-header border-secondary">
-                        <div class="row">
-                            <div class="col-10">
-                                <a href="<?php echo base_url("/usuarios_c/perfil/{$aviso->nombre_usu}") ?>">
-                                    <?php if ($aviso->img_usu == "") : ?>
-                                        <img style="width: 70px;" class="rounded-circle z-depth-0" src="<?php echo base_url('assets/img/usuario.jpg') ?>"></a>
-                                <?php else : ?>
-                                    <img style="width: 70px;" class="rounded-circle z-depth-0" src="<?php echo base_url($aviso->img_usu) ?>"></a>
-                                <?php endif; ?>
-                                <span class="h5 text-primary"><?php echo $aviso->nombre_usu ?></span>
-                            </div>
-                            <div class="col-2">
-                                <span class="text-right"><?php echo $aviso->fecha_an ?></span>
-                                <?php if ($aviso->usu_an == $_SESSION['id_login']) ?>
-                                <button class="btn btn-outline-danger borrarAviso waves-effect" id="<?php echo $aviso->id_an ?>" type="button">
-                                    <i class="fas fa-trash-alt"></i></button>
-                            </div>
+            <div class="card border-secondary p-2 m-2">
+                <div class="card-header border-secondary">
+                    <div class="row">
+                        <div class="col-10">
+                            <a href="<?php echo base_url("/usuarios_c/perfil/{$aviso->nombre_usu}") ?>">
+                                <?php if ($aviso->img_usu == "") : ?>
+                                <img style="width: 70px;" class="rounded-circle z-depth-0"
+                                    src="<?php echo base_url('assets/img/usuario.jpg') ?>"></a>
+                            <?php else : ?>
+                            <img style="width: 70px;" class="rounded-circle z-depth-0"
+                                src="<?php echo base_url($aviso->img_usu) ?>"></a>
+                            <?php endif; ?>
+                            <div class="h5 text-primary"><?php echo $aviso->nombre_usu ?></div>
+                        </div>
+                        <div class="col-2">
+                            <span class="text-right"><?php echo $aviso->fecha_an ?></span>
+                            <?php if ($aviso->usu_an == $_SESSION['id_login']) : ?>
+                            <button class="btn btn-outline-danger borrarAviso waves-effect"
+                                id="<?php echo $aviso->id_an ?>" type="button">
+                                <i class="fas fa-trash-alt"></i></button>
+                            <?php endif; ?>
                         </div>
                     </div>
-                    <div class="card-body text-secondary">
-                        <h5 class="card-title"><?php echo $aviso->titulo_an ?></h5>
-                        <p class="card-text"><?php echo $aviso->cuerpo_an ?></p>
-                    </div>
                 </div>
+                <div class="card-body text-secondary">
+                    <h5 class="card-title"><?php echo $aviso->titulo_an ?></h5>
+                    <p class="card-text"><?php echo $aviso->cuerpo_an ?></p>
+                </div>
+            </div>
             <?php endforeach; ?>
         </div>
     </div>
     <form action="<?php echo base_url('Avisos_c/crear') ?>" method="post">
-        <div class="modal fade" id="modalCrearAviso" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal fade" id="modalCrearAviso" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+            aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header text-center">
@@ -71,7 +76,8 @@
                         <!--Textarea with icon prefix-->
                         <div class="md-form mb-5 amber-textarea active-amber-textarea-2">
                             <i class="fas fa-pencil-alt prefix"></i>
-                            <textarea id="cuerpo_an" name="cuerpo_an" class="md-textarea form-control" rows="3" required></textarea>
+                            <textarea id="cuerpo_an" name="cuerpo_an" class="md-textarea form-control" rows="3"
+                                required></textarea>
                             <label for="cuerpo_an">Cuerpo del aviso</label>
                         </div>
                     </div>
